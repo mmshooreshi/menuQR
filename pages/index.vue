88f8cb0a4cfb77d0508aff8fd28d252b88c850d5
Mam Thenebo <mmshooreshi@gmail.com>
2024-01-05 15:02:28 +0330

    s

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        <!-- Page content -->
-        Hey
+        <ul>
+            <li v-for="slug in slugs" :key="slug">
+                <nuxt-link :to="`/menus/${slug}`">{{ slug }}</nuxt-link>
+            </li>
+        </ul>
     </div>
 </template>
 
-<script setup>
-// Component logic
+<script>
+export default {
+    async asyncData({ $http }) {
+        // Fetch the list of PDF slugs from the static JSON file
+        try {
+            const slugs = await $http.$get('/pdfList.json');
+            return { slugs };
+        } catch (e) {
+            // Handle error (e.g., file not found)
+            return { slugs: [] };
+        }
+    }
+}
 </script>
 
 <style scoped>
